2e08a05c5efee78130f768d025ada9a5e66ba9dc
vuen-asus <saurek115@gmail.com>
2022-07-14 20:44:33 -0500

    add new dashboard stuff

---
+++ comfyai/app/create.js
@@ -0,0 +1,15 @@
+const events_data = {
+    title: 'Event Options',
+    desc: 'Select from the event options below to proceed to the next step.',
+    types: [
+        {
+            type: 'reaction',
+            desc: 'Triggers when a message is reacted to in your server.'
+        },
+        {
+            type: 'message',
+            desc: 'Triggers when a message is sent in your server.'
+        }
+
+    ]
+}
